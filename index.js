25b7d91a1ef359e8c517c796133eea74fff186c4
TANMAY TIWARI 1.2 <tanmaytiwaricyber@gmail.com>
2024-10-16 21:13:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ document.addEventListener("contextmenu", function (e) {
         e.preventDefault();
         alert('Access Denied to see source code !! ');
 }, false);
+
+function toggleMenu() {
+        const navLinks = document.getElementById("navLinks");
+        if (navLinks.style.display === "flex") {
+            navLinks.style.display = "none";
+        } else {
+            navLinks.style.display = "flex";
+        }
+    }
+    
